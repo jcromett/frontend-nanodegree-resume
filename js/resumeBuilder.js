4c8b777ca442c958c4e489e1a1a59ec6757267a9
jcromett <jcromett@clarity-innovations.com>
2017-03-29 17:50:24 -0700

    Add var for Awesomeness

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append("John Cromett");
+
+var awesomeThoughts = "I am [name] and I am AWESOME";
+var awesomerThoughts = awesomeThoughts.replace ("[name]", "John");
+
+console.log(awesomeThoughts);
+console.log(awesomerThoughts);
